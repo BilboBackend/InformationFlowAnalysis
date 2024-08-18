63c118bb7f1b7e00ffa279716fc1c36cc9d0ada3
Oskar H Dinesen <oskar.h.dinesen@gmail.com>
2024-08-18 13:52:14 +0200

    Fixed grammar for arithmetic expressions to be right-recursive and non-ambiguous, and implemented a parser for arithmetic expressions

--- app/AST.hs
+++ app/AST.hs
@@ -7,12 +7,19 @@ import Data.HashMap.Strict as HM
 data C = Low | High
 
 type Var = String
-data AExpr = N Integer | X Var  | Plus AExpr AExpr | Mult AExpr AExpr | Minus AExpr AExpr -- | Div AExpr AExpr
-data BExpr = T | And BExpr BExpr | Or BExpr BExpr | Neg BExpr | Lt AExpr AExpr | Leq AExpr AExpr| Eq AExpr AExpr
 
+data AExpr = Plus ATerm AExpr | Minus ATerm AExpr | Term ATerm deriving Show 
+data ATerm = Mult AFact ATerm| Fact AFact deriving Show 
+data AFact = Paren AExpr | Atom AAtom deriving Show 
+data AAtom = N Integer | X Var deriving Show 
+
+data BExpr = T | And BExpr BExpr | Or BExpr BExpr | Neg BExpr | Lt AExpr AExpr | Leq AExpr AExpr| Eq AExpr AExpr
+  deriving Show
 
 data Stmt = Assign Var AExpr | Input Var Var | Output AExpr Var | IfThenElse BExpr Stmt Stmt | While BExpr Stmt | Seq Stmt Stmt
+  deriving Show
 
+type Code = [Stmt]
 type Memory = HashMap Var Integer
 
 -- define member function for AST, and check whether all variables are in memory 
@@ -24,47 +31,61 @@ safelookup v mem = case HM.lookup v mem of
   Just i -> i 
   Nothing -> 0
 
-semA :: AExpr -> Memory -> Integer
-semA expr mem = case expr of 
-  N i -> i  
-  X v -> safelookup v mem 
-  Plus x y -> (semA x mem) + (semA y mem)
-  Mult x y -> (semA x mem) * (semA y mem)
-  Minus x y -> (semA x mem) - (semA y mem) 
- 
+semAA :: AAtom -> Memory -> Integer 
+semAA atom mem = case atom of 
+  N i -> i 
+  X v -> safelookup v mem
 
-semB :: BExpr -> Memory -> Bool
-semB expr mem = case expr of
-  T -> True 
-  And x y -> (semB x mem) && (semB y mem) 
-  Or x y -> (semB x mem) || (semB y mem) 
-  Neg x -> not (semB x mem) 
-  Lt x y -> (semA x mem) < (semA y mem)
-  Leq x y -> (semA x mem) <= (semA y mem)
-  Eq x y -> (semA x mem) == (semA y mem)
+semAF :: AFact -> Memory -> Integer 
+semAF fact mem = case fact of 
+  Paren expr -> semAE expr mem
+  Atom a -> semAA a mem
 
-sem :: Stmt -> Memory -> Memory 
-sem statement mem = case statement of 
-    Assign v aexpr -> HM.insert v (semA aexpr mem) mem 
-    Input v1 v2 -> HM.insert v2 (safelookup v1 mem) mem 
-    Output aexpr v ->  HM.insert v (semA aexpr mem) mem
-    IfThenElse bexpr s1 s2 -> if
-      (semB bexpr mem) == True then 
-        sem s1 mem 
-      else sem s2 mem  
-    While bexpr s -> if 
-      (semB bexpr mem == True) then 
-        let mem2 = sem s mem in sem (While bexpr s) mem2 
-        else mem 
-    Seq s1 s2 -> sem s2 (sem s1 mem) 
+semAT :: ATerm -> Memory -> Integer
+semAT term mem = case term of 
+  Mult x y -> (semAF x mem)* (semAT y mem)
+  Fact x -> semAF x mem
 
+semAE :: AExpr -> Memory -> Integer
+semAE expr mem = case expr of 
+  Plus x y -> (semAT x mem) + (semAE y mem)
+  Minus x y -> (semAT x mem) - (semAE y mem) 
+  Term x -> (semAT x mem) 
 
--- check addition and assign
-check1 = Assign ("a" :: Var) (Plus (N 2) (N 3))
-memory1 = HM.fromList [("a",2)] 
-
--- check simple counting while
-emptymem = HM.empty 
-check2 = Seq (Assign ("a" :: Var) (N 0)) (While (Lt (X "a") (N 3)) (Assign ("a" :: Var) (Plus (X "a") (N 1))))
-
-
+executeAE :: AExpr -> Integer
+executeAE expr = semAE expr HM.empty 
+-- semB :: BExpr -> Memory -> Bool
+-- semB expr mem = case expr of
+--   T -> True 
+--   And x y -> (semB x mem) && (semB y mem) 
+--   Or x y -> (semB x mem) || (semB y mem) 
+--   Neg x -> not (semB x mem) 
+--   Lt x y -> (semA x mem) < (semA y mem)
+--   Leq x y -> (semA x mem) <= (semA y mem)
+--   Eq x y -> (semA x mem) == (semA y mem)
+--
+-- sem :: Stmt -> Memory -> Memory 
+-- sem statement mem = case statement of 
+--     Assign v aexpr -> HM.insert v (semA aexpr mem) mem 
+--     Input v1 v2 -> HM.insert v2 (safelookup v1 mem) mem 
+--     Output aexpr v ->  HM.insert v (semA aexpr mem) mem
+--     IfThenElse bexpr s1 s2 -> if
+--       (semB bexpr mem) == True then 
+--         sem s1 mem 
+--       else sem s2 mem  
+--     While bexpr s -> if 
+--       (semB bexpr mem == True) then 
+--         let mem2 = sem s mem in sem (While bexpr s) mem2 
+--         else mem 
+--     Seq s1 s2 -> sem s2 (sem s1 mem) 
+--
+--
+-- -- check addition and assign
+-- check1 = Assign ("a" :: Var) (Plus (N 2) (N 3))
+-- memory1 = HM.fromList [("a",2)] 
+--
+-- -- check simple counting while
+-- emptymem = HM.empty 
+-- check2 = Seq (Assign ("a" :: Var) (N 0)) (While (Lt (X "a") (N 3)) (Assign ("a" :: Var) (Plus (X "a") (N 1))))
+--
+--
--- app/Parser.hs
+++ app/Parser.hs
@@ -1,9 +1,61 @@
-import AST 
+module Parser where
+
+import AST as AST 
 
 import Text.Parsec 
 import Text.Parsec.String
-import Text.ParserCombinators.Parsec.Number 
 
 
+assignParser :: Parser Stmt
+assignParser = Assign <$> (many1 letter) <*> (Term <$> Fact <$> Atom <$> N <$> read <$> (spaces >> char ':' >> char '=' >> spaces >> many1 digit))
 
+aexprParser :: Parser AExpr 
+aexprParser =  try plusParser <|> try minusParser <|> try (Term <$> termParser)
 
+plusParser :: Parser AExpr 
+plusParser = Plus <$> termParser <*> (spaces >> char '+' >> spaces >> aexprParser)
+
+minusParser :: Parser AExpr 
+minusParser = Minus <$> termParser <*> (spaces >> char '-' >> spaces >> aexprParser) 
+
+termParser :: Parser ATerm 
+termParser = try multParser <|> Fact <$> factorParser
+
+multParser :: Parser ATerm 
+multParser =  Mult <$> factorParser <*> (spaces >> char '*' >> spaces >> termParser)
+
+factorParser :: Parser AFact 
+factorParser = try parenParser <|> Atom <$> atomParser 
+
+parenParser :: Parser AFact 
+parenParser = Paren <$> (do char '(' >> spaces
+                            expr <- aexprParser 
+                            spaces >> char ')' 
+                            return expr)
+
+atomParser :: Parser AAtom 
+atomParser = X <$> (many1 letter) <|> N <$> (read <$> many1 digit)
+--
+--
+-- boolParser :: Parser BExpr 
+-- boolParser = (T <$ (string "True")) <|> (Neg <$> (T <$ (string "False")))
+--
+-- negParser :: Parser BExpr 
+-- negParser = Neg <$> (string "not" >> spaces >> bexprParser)
+--
+-- bexprParser :: Parser BExpr
+-- bexprParser = negParser <|> ltParser <|> leqParser <|> eqParser <|> boolParser 
+--
+-- leqParser :: Parser BExpr
+-- leqParser = Leq <$> aexprParser <*> ( spaces >> string "<=" >> spaces >> aexprParser)
+--
+--
+-- ltParser :: Parser BExpr
+-- ltParser = Lt <$> aexprParser <*> ( spaces >> char '<' >> spaces >> aexprParser)
+--
+-- eqParser :: Parser BExpr
+-- eqParser = Eq <$> dataParser <*> ( spaces >> char '=' >> spaces >> dataParser)
+--
+--
+--
+--   
